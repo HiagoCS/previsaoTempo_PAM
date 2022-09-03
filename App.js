2b9ee6859da70f334dfae37ea9d4679b2da18471
HiagoCS <hiago8cs@gmail.com>
2022-09-03 16:05:25 -0300

    Exibição do dia atual completa

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Dimensions, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, TextInput, ScrollView } from 'react-native';
 import { useState } from 'react';
 import Header from './components/header';
 import Body from './components/body';
@@ -7,9 +7,11 @@ import Api from './components/api'
 export default function App() {
   const [cidade, setCidade] = useState("");
   const [dados, setDados] = useState("");
+  const [bodyOpc, setBodyOpc] = useState(1);
   async function buscaCep(){
-    const response = await Api.get(`weather?array_limit=1&fields=only_results,temp,city_name,time,forecast,max,min,date,description&key=13ada48a&city_name=${cidade},SP`);
+    const response = await Api.get(`weather?array_limit=8&fields=only_results,temp,city_name,time,forecast,max,min,date,description,humidity,condition_slug&key=13ada48a&city_name=${cidade},SP`);
     setDados(response.data);
+    setBodyOpc(1);
     console.log("Sucesso!!");
   }
   return (
@@ -26,7 +28,11 @@ export default function App() {
     ></Header>
       </View>
       <View style={styles.body}>
-        <Body data={dados}></Body>
+          <ScrollView
+          horizontal>
+            <Body opacity={bodyOpc} data={dados}></Body>
+            <Body opacity={bodyOpc} data={dados}></Body>
+          </ScrollView>
       </View>
     </View>
   );
@@ -39,6 +45,8 @@ const styles = StyleSheet.create({
   },
   body:{
     marginTop:Dimensions.get('window').height / 6,
+    backgroundColor:'#71ACAF',
+    height:Dimensions.get('window').height
   },
   input:{
     width:Dimensions.get('window').width / 1.6,
--- components/body.js
+++ components/body.js
@@ -1,11 +1,120 @@
 import { StyleSheet, Text, View, Dimensions} from 'react-native';
+import { Icon } from 'react-native-elements';
 
 export default props=>{
     return(
-        <Text>{props.teste}</Text>
+        <View style={{opacity:props.opacity, fontFamily:'Roboto', flexDirection:'column', alignItems:'center'}}>
+            <View style={{marginRight:Dimensions.get('window').width / 2}}>
+                <View style={styles.date}>
+                    <Text style={styles.dateFont}>{props.data.date}, {props.data.time}</Text>
+                </View>
+                <View style={styles.max_min}>
+                    <View style={styles.max}>
+                        <Text style={styles.maxFont}>Max: {props.data.temp}°</Text>
+                        <Icon style={styles.maxIcon} size={15} color={'white'} name='north'/>
+                    </View>
+                    <View style={styles.min}>
+                        <Text style={styles.minFont}>Min: {props.data.temp}°</Text>
+                        <Icon style={styles.minIcon} size={15} color={'white'} name='south'/>
+                    </View>
+                </View>
+                <View style={styles.temp}>
+                    <Text style={styles.tempFont}>{props.data.temp}</Text>
+                    <Text style={styles.tempIcon}>°C</Text>
+                </View>
+            </View>
+            <View style={styles.rightWindow}>
+                <View style={styles.descrip}>
+                    <Text style={styles.descripFont}>{props.data.description}</Text>
+                </View>
+            </View>
+            <View style={styles.centerWindow}>
+                <View style={styles.humidity}>
+                    <Text style={styles.humidityFont}>Humidade atual: ({props.data.humidity}%)</Text>
+                </View>
+            </View>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    rightWindow:{
+        marginLeft:Dimensions.get('window').width / 2,
+        position:'relative',
+        bottom:30
+    },
+    centerWindow:{
+        alignItems:'center',
+        justifyContent:'center',
+        marginTop:Dimensions.get('window').height /3
+    },
+    date:{
+        marginLeft:25,
+        marginTop:5
+    },
+    dateFont:{
+        fontSize:20,
+        fontWeight:'bold',
+        color:'#212237',
+        textShadowColor: 'rgba(255, 255, 255, 0.75)',
+        textShadowOffset: {width: -1, height: 1},
+        textShadowRadius: 10
+    },
+    max_min:{
+        flexDirection:'row',
+        marginLeft:25,
+        marginTop:15
+    },
+    maxFont:{
+        color:'white',
+        fontWeight:'bold',
+        fontSize:20,
+        textShadowColor: 'rgba(0, 0, 0, 0.75)',
+        textShadowOffset: {width: -1, height: 1},
+        textShadowRadius: 10
+    },
+    minFont:{
+        color:'white',
+        fontWeight:'bold',
+        fontSize:20,
+        marginLeft:20,
+        textShadowColor: 'rgba(0, 0, 0, 0.75)',
+        textShadowOffset: {width: -1, height: 1},
+        textShadowRadius: 10
+    },
+    temp:{
+        alignItems:'center',
+        flexDirection:'row'
+    },
+    tempFont:{
+        fontSize:100,
+        color:'white',
+        textShadowColor: 'rgba(0, 0, 0, 0.75)',
+        textShadowOffset: {width: -1, height: 1},
+        textShadowRadius: 10
+    },
+    tempIcon:{
+        fontSize: 50,
+        color:'white',
+        marginBottom:20,
+        textShadowColor: 'rgba(0, 0, 0, 0.75)',
+        textShadowOffset: {width: -1, height: 1},
+        textShadowRadius: 10
+    },
+    descripFont:{
+        fontWeight:'bold',
+        fontSize:15,
+        color:'white',
+        textShadowColor: 'rgba(0, 0, 0, 0.75)',
+        textShadowOffset: {width: -1, height: 1},
+        textShadowRadius: 10
+    },
+    humidityFont:{
+        fontWeight:'bold',
+        color:'white',
+        fontSize:25,
+        textShadowColor: 'rgba(0, 0, 0, 0.75)',
+        textShadowOffset: {width: -1, height: 1},
+        textShadowRadius: 10
+    }
 });
